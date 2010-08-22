cee8f47f51f05e1236a9cdf725efcfb0095d9841
stef <stefan.marsiske@gmail.com>
2010-08-23 01:19:17 +0200

    [enh] added vector class + gravity to initial movement calculation.

--- swarm.py
+++ swarm.py
@@ -2,64 +2,66 @@
 
 import random
 from math import sqrt
-from itertools import imap
-from operator import add, sub
-from time import sleep
 
 # number of swarms
 SN = 100
 
-class Swarm:
-    def __init__(self, weight=1, avoid=0.3, attract=6, resolution=1):
-        self.x          = (random.random()-0.5)*pow(10, resolution)*2
-        self.y          = (random.random()-0.5)*pow(10, resolution)*2
-        self.z          = (random.random()-0.5)*pow(10, resolution)*2
-        self.weight     = weight
+class Vector():
+    def __init__(self,x=0,y=0,z=0):
+        self.x=x
+        self.y=y
+        self.z=z
+
+    def dist(self,other=None):
+        if not other:
+            other=Vector()
+        return sqrt(pow(self.x-other.x, 2)+pow(self.y-other.y, 2)+pow(self.z-other.z, 2))
+
+    def normalize(self, d):
+        if not d==0:
+            self.x/=d
+            self.y/=d
+            self.z/=d
+        return self
+
+    def gravity(self, other, d, c):
+        g=c/pow(d, 2)
+        return Vector((self.x-other.x)*g/d, (self.y-other.y)*g/d, (self.z-other.z)*g/d)
+
+    def add(self,other):
+        self.x+=other.x
+        self.y+=other.y
+        self.z+=other.z
+
+    def __str__(self):
+        return '%f %f %f' % (self.x/100, self.y/100, self.z/100)
+
+class Agent:
+    def __init__(self, avoid=(0, 20), attract=(50,300)):
+        self.pos        = Vector((random.random()-0.5)*800,(random.random()-0.5)*800,(random.random()-0.5)*800)
         self.avoid      = avoid
         self.attract    = attract
 
-    def __str__(self): return '%.4f %.4f %.4f' % (self.x, self.y, self.z)
+    def __str__(self):
+        return str(self.pos)
 
-    def add(self, b):
-        self.x += b[0]
-        self.y += b[1]
-        self.z += b[2]
-        return [self.x, self.y, self.z]
+    def move(self,swarm):
+        v=Vector()
+        p=self.pos
+        for agent in swarm:
+            d=self.pos.dist(agent.pos)
+            if self.avoid[0] < d <=self.avoid[1]:
+                v.add(p.gravity(agent.pos,d,10))
+            elif self.attract[0] < d <=self.attract[1]:
+                v.add(p.gravity(agent.pos,d,-1000))
+        self.pos.add(v.normalize(v.dist()))
 
-def distance(a, b):
-    return sqrt(pow(a.x-b.x, 2)+pow(a.y-b.y, 2)+pow(a.z-b.z, 2))
-
-def gravity(d):
-    return 1/pow(d, 2)
-
-def movingG(a, b, d):
-    return [(a.x-b.x)/d*gravity(d), (a.y-b.y)/d*gravity(d), (a.z-b.z)/d*gravity(d)]
-
-def moving(a, b, d):
-    return [(a.x-b.x)/d, (a.y-b.y)/d, (a.z-b.z)/d]
-
-def genNext(world):
-    dm = [[distance(a,b) for a in world] for b in world]
-    for i,swarm in enumerate(world):
-        move = [0, 0, 0]
-        c = 0
-        for j,dist in enumerate(dm[i]):
-            if i == j or dist == 0:
-                continue
-            if dist > swarm.avoid and dist < swarm.attract:
-                c+=1
-                move = [x for x in imap(sub, move, moving(swarm, world[j], dist))]
-            elif dist < swarm.avoid:
-                c+=1
-                move = [x for x in imap(add, move, moving(swarm, world[j], dist))]
-        if c:
-            world[i].add([x/c for x in move])
+def nextframe(swarm):
+    return [a.move(swarm) for a in swarm]
 
 if __name__ == '__main__':
-    world = [Swarm() for x in range(SN)]
-    print '\n'.join(map(str, world))+'\ndone'
-    for i in range(1000):
-        genNext(world)
-        sleep(0.15)
-        print '\n'.join(map(str, world))+'\ndone'
-       
+    swarm = [Agent() for x in range(SN)]
+    print '\n'.join(map(str, swarm))+'\ndone'
+    for i in range(2000):
+        nextframe(swarm)
+        print '\n'.join(map(str, swarm))+'\ndone'
