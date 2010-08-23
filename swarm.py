cad9aa7345a7c33167bc0fea30335b4d484e505c
stef <stefan.marsiske@gmail.com>
2010-08-23 20:01:51 +0200

    [enh] added mating+feeding zones, hunger and other tweaks

--- swarm.py
+++ swarm.py
@@ -4,8 +4,8 @@ import random
 from math import sqrt
 
 # number of swarms
-Fishes = 100
-Sharks = 2
+Fishes = 50
+Sharks = 4
 Planktii = 100
 VIEWPORT = 100
 
@@ -41,73 +41,90 @@ class Vector():
 
 class Agent:
     def __str__(self):
-        #return "%s" % (self.vitality)
+        #return "%s %s" % (self.species, self.vitality)
         if self.vitality>0:
-            return "%s %s" % (str(self.pos), self.race)
+            return "%s %s" % (str(self.pos), self.species)
         else:
             return ''
 
     def move(self,swarm):
+        if self.vitality<=0: return
         v=Vector()
         p=self.pos
         for agent in swarm:
             d=self.pos.dist(agent.pos)
-            for zone in self.zones[agent.race]:
-                if zone['min'] < d <= zone['max']:
+            # calculate vector of next move
+            for zone in self.zones[agent.species]:
+                if zone['min']/self.vitality < d <= zone['max']/self.vitality:
                     v.add(p.gravity(agent.pos,d,zone['c']))
             # handle health
-            if agent.race != self.race:
-                if d<5:
-                    # do damage/nutrition
-                    self.vitality+=self.nutrition[agent.race]
-            elif d<self.matingZone and self.vitality>1:
+            if agent.species == self.prey and d<self.eatingZone and agent.vitality>0:
+                # do damage/nutrition
+                self.vitality+=self.nutrition[agent.species]
+                agent.vitality+=agent.nutrition[self.species]
+            elif agent.species == self.species and self.vitality>=1 and d<self.matingZone:
                 # give birth
                 Nursery.append(self.birth())
                 self.vitality=0.7
+            if self.vitality>2:
+                # glutony results in emptying everything
+                self.vitality=0.5
+            else:
+                self.vitality+=self.hunger
         self.pos.add(v.normalize(v.dist(),self.vitality*self.speed))
 
 class Fish(Agent):
     def __init__(self):
+        self.species    = 0
+        self.speed      = 1
+        self.vitality   = 0.7
         self.pos        = Vector((random.random()-0.5)*1000,(random.random()-0.5)*1000,(random.random()-0.5)*1000)
         self.zones      = [[{'min': 0, 'max': 20, 'c': 100 }, {'min': 50, 'max': 300, 'c': -1000 }],
-                           [{'min': 0, 'max': 700, 'c': 1000 }],
-                           [{'min': 0, 'max': 1000, 'c': -1000 }]]
-        self.matingZone = 30
-        self.speed      = 1
-        self.race       = 0
-        self.vitality   = 0.999999999999
+                           [{'min': 0, 'max': 700, 'c': 1300 }],
+                           [{'min': 0, 'max': 1000, 'c': -1100 }]]
+        self.matingZone = 80
+        self.eatingZone = 40
+        self.prey       = 2
         self.nutrition  = {1: -0.7,
-                           2: 0.4}
+                           2: 0.2}
+        self.hunger     = -0.000001
 
     def birth(self):
         return Fish()
 
 class Shark(Agent):
     def __init__(self):
+        self.species    = 1
+        self.speed      = 3
+        self.vitality   = 0.8
         self.pos        = Vector((random.random()-0.5)*1000,(random.random()-0.5)*1000,(random.random()-0.5)*1000)
         self.zones      = [[{'min': 0, 'max': 600, 'c': -1200 }],
                            [{'min': 0, 'max': 100, 'c': 10 }, {'min': 200, 'max': 800, 'c': -1000 }],
-                           [{'min': 0, 'max': 20, 'c': 1 }]]
-        self.matingZone = 0.0000001
-        self.speed      = 2
-        self.race       = 1
-        self.vitality   = 0.8
-        self.nutrition  = {0: 0.1,
-                           2: -0.3}
+                           [{'min': 0, 'max': 140, 'c': 800 }]]
+        self.matingZone = 60
+        self.eatingZone = 15
+        self.prey       = 0
+        self.nutrition  = {0: 0.05,
+                           2: -0.01}
+        self.hunger     = -0.000001
+
     def birth(self):
         return Shark()
 
 class Plankton(Agent):
     def __init__(self):
+        self.species    = 2
+        self.speed      = 0.1
+        self.vitality   = 0.5
         self.pos        = Vector((random.random()-0.5)*1000,(random.random()-0.5)*1000,(random.random()-0.5)*1000)
         self.zones      = [[{'min': 0, 'max': 5, 'c': 10 }],
                            [{'min': 0, 'max': 1000, 'c': -1000 }],
                            []]
-        self.matingZone = 1000000000000000
-        self.speed      = 0.1
-        self.race       = 2
-        self.vitality   = 1
-        self.nutrition  = {0: -1, 1: 0.8}
+        self.matingZone = 100
+        self.eatingZone = 70
+        self.prey       = 1
+        self.nutrition  = {0: -0.1, 1: 0.3}
+        self.hunger     = -0.000001
 
     def birth(self):
         return Plankton()
