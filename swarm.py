a77d3cd80d6855a2a9331fdb66d4ffc140cedad0
stef <stefan.marsiske@gmail.com>
2010-09-03 04:30:29 +0200

    [enh] eating and mating depends on zone

--- swarm.py
+++ swarm.py
@@ -3,6 +3,7 @@
 import sys
 from math import sqrt
 from species import *
+from random import gauss, random
 
 VIEWPORT = 100
 
@@ -25,8 +26,8 @@ class Vector():
         return self
 
     def gravity(self, other, d, c):
-        g=c/pow(d, 2)
-        return Vector((self.x-other.x)*g/d, (self.y-other.y)*g/d, (self.z-other.z)*g/d)
+        g=c/pow(d, 4)
+        return Vector((self.x-other.x)*g, (self.y-other.y)*g, (self.z-other.z)*g)
 
     def add(self,other):
         self.x+=other.x
@@ -39,6 +40,16 @@ class Vector():
 class Agent:
     def __str__(self):
         return "%s %s" % (str(self.pos), self.species)
+
+    def __init__(self,parent=None):
+        self.default() # set species defaults
+        if parent:
+            # handle mutations
+            self.speed      = parent.speed*gauss(1, 0.05)
+            self.matingZone = parent.matingZone * gauss(1, 0.05)
+            self.eatingZone = parent.eatingZone * gauss(1, 0.05)
+            self.hunger     = parent.hunger*self.speed*gauss(1,0.05)
+            self.nutrition  = dict([(key,value*gauss(1,0.05)) for (key,value) in parent.nutrition.items()])
 
     def move(self,swarm,world):
         if self.vitality<=0: return
@@ -53,18 +64,25 @@ class Agent:
             # handle health
             if agent.species == self.prey and d<self.eatingZone and agent.vitality>0:
                 # do damage/nutrition
-                self.vitality+=self.nutrition[agent.species]
-                agent.vitality+=agent.nutrition[self.species]
+                c=(1-(d/self.eatingZone)) # how much we bite?
+                self.vitality+=self.nutrition[agent.species]*c
+                agent.vitality+=agent.nutrition[self.species]*c
             elif agent.species == self.species and self.vitality>=1 and d<self.matingZone:
-                # breed
-                world.swarm[self.species].append(self.breed())
-                self.vitality=0.7
+                # try to breed
+                c=(1-(d/self.matingZone)) # how much we the chance we breed?
+                if(random()<c*(self.vitality**2)):
+                    world.swarm[self.species].append(self.breed())
+                    self.vitality=0.7
             if self.vitality>2:
                 # glutony results in emptying everything
                 self.vitality=0.5
             else:
                 self.vitality+=self.hunger
-        self.pos.add(v.normalize(v.dist(),self.vitality*self.speed))
+        max_move=v.normalize(v.dist(),(0.5+self.vitality/2)*self.speed)
+        if v.dist()>max_move.dist():
+            self.pos.add(max_move)
+        else:
+            self.pos.add(v)
 
 class World():
     def __init__(self):
